8875a71a1ff9bf4ed2884a255e754d9748f92ae6
Yehor <yehor@MacBook-Pro-Yehor.local>
2025-09-14 10:12:06 +0200

    update

--- client/src/components/calendarAdmin/CalendarAdmin.jsx
+++ client/src/components/calendarAdmin/CalendarAdmin.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { DayPicker } from "react-day-picker";
+import { ru } from "date-fns/locale";
 import "react-day-picker/dist/style.css";
 import "./CalendarAdmin.css";
 
@@ -63,6 +64,7 @@ export default function AdminCalendar({
           numberOfMonths={months}
           weekStartsOn={weekStartsOn}
           selected={selected}
+          locale={ru} 
           onSelect={readOnly ? undefined : onSelectRange}
           // показываем и подсвечиваем занятые ночи
           modifiers={{ booked }}
